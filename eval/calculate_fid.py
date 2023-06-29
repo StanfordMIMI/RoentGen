a9e5b65b18823760dcd0be10bd3061a38e582524
bluch <chris.bluethgen@gmail.com>
2023-06-29 17:56:19 +0200

    add eval scripts

--- eval/calculate_fid.py
+++ eval/calculate_fid.py
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 consoleHandler = logging.StreamHandler()
 consoleHandler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s; %(levelname)s: %(message)s')
 consoleHandler.setFormatter(formatter)
 logger.addHandler(consoleHandler)
 
